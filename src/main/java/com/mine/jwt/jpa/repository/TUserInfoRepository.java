bb73037b27dbee7b54cc0d06f8943e4f183ef998
zmf123456 <415073413@qq.com>
2018-09-11 18:42:01 +0800

    update

---
+++ src/main/java/com/mine/jwt/jpa/repository/TUserInfoRepository.java
@@ -0,0 +1,16 @@
+package com.mine.jwt.jpa.repository;
+
+import com.mine.jwt.jpa.entity.TUserInfo;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Title:
+ * Description:
+ * Copyright: Copyright (c) 2018
+ *
+ * @author Murphy.Chang
+ * @version 1.0
+ * @date 2018/5/29
+ */
+public interface TUserInfoRepository extends CrudRepository<TUserInfo, Integer> {
+}
